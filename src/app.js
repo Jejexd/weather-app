72a20aee29b3f62e53a0587b61c4ebc2849385d5
Jejexd <jenireecastro@gmail.com>
2022-01-16 22:55:44 -0600

    Updated JS and CSS

--- src/app.js
+++ src/app.js
@@ -21,16 +21,42 @@ function dateFormat(event) {
   let dateNow = `${day}  ${hour}:${minute}`;
 
   return dateNow;
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
 }
 let eventElement = document.querySelector("#date");
 let currentTime = new Date();
 eventElement.innerHTML = dateFormat(currentTime);
 
+function getForecast(coordinates) {
+  let apiKey = "056ff5c21ea93f2f14b864eb4f4a37a4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function Weather(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
